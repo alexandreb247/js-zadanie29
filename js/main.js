e8cf7f3476d4f40cd838d330771fb8ef8612ec20
alexandreb247 <alexandre@poczta.onet.pl>
2022-10-07 11:05:33 +0200

    zadanie wykonane

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var firstNumber;
+var secondNumber;
+var suma;
+
+function dodawanie(firstNumber, secondNumber) {
+    suma = firstNumber + secondNumber;
+    return suma;
+  }
+console.log(dodawanie(3,1));
